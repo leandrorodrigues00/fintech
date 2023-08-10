0af4a49848b44e51790bd781a5545677484abe87
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2023-08-10 08:47:42 -0300

    feat CSS base

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import "./style.css";
 export function App() {
   return (
     <DataContextProvider>
-      <div>
+      <div className="container">
         <Sidenav />
         <main>
           <Header />
--- src/components/date-range.tsx
+++ src/components/date-range.tsx
@@ -5,20 +5,18 @@ export function DateRange() {
   const [inicio, setInicio] = useState("");
   const [final, setFinal] = useState("");
   return (
-    <form onSubmit={(event) => event.preventDefault()}>
+    <form className="box flex" onSubmit={(event) => event.preventDefault()}>
       <DateInput
         label="Início"
         value={inicio}
         onChange={(event) => setInicio(event.target.value)}
       />
 
-      {inicio}
       <DateInput
         label="Final"
         value={final}
         onChange={(event) => setFinal(event.target.value)}
       />
-      {final}
     </form>
   );
 }
